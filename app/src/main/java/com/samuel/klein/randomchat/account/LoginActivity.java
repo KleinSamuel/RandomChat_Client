debdc813f19f9286e0c544e6b8dc25f7ed01582d
Samuel Klein <samuel.klein@online.de>
2017-11-05 19:23:46 +0100

    changed login activity for better handling with main activity

--- app/src/main/java/com/samuel/klein/randomchat/account/LoginActivity.java
+++ app/src/main/java/com/samuel/klein/randomchat/account/LoginActivity.java
@@ -3,58 +3,138 @@ package com.samuel.klein.randomchat.account;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.Socket;
 import com.samuel.klein.randomchat.R;
-import com.samuel.klein.randomchat.chat.ChatTestActivity;
+import com.samuel.klein.randomchat.debug.Debug;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Socket mSocket;
 
     private EditText usernameInput;
     private EditText passwordInput;
 
     private Button loginButton;
-    private Button switchButton;
+
+    private String mUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        ChatApplication app = (ChatApplication) getApplication();
+        mSocket = app.getSocket();
+
+        mSocket.connect();
+
         usernameInput = (EditText) findViewById(R.id.username_input);
         passwordInput = (EditText) findViewById(R.id.password_input);
 
         loginButton = (Button) findViewById(R.id.login_button);
-        switchButton = (Button) findViewById(R.id.switch_button);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //System.out.println("Username: "+usernameInput.getText());
-                //System.out.println("Password: "+passwordInput.getText());
-                login();
+        loginButton.setOnClickListener(loginListener);
+
+        mSocket.on("loginResponse", onLogin);
+
+    }
+
+    /**
+     * Simple validation if user entered something
+     *
+     * @param username
+     * @param password
+     * @return true if user entered username and password
+     */
+    private boolean validateInput(String username, String password){
+
+        if(TextUtils.isEmpty(username)) {
+            usernameInput.setError(getString(R.string.error_field_required));
+            usernameInput.requestFocus();
+            return false;
+        }
+        if(TextUtils.isEmpty(password)) {
+            passwordInput.setError(getString(R.string.error_field_required));
+            passwordInput.requestFocus();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Send login request to server
+     *
+     * @param username
+     * @param password
+     */
+    private void login(String username, String password){
+
+        Debug.print("Trying to login to server [username="+username+"] ..");
+
+        try {
+
+            JSONObject object = new JSONObject();
+            object.put("username", username);
+            object.put("password", password);
+            mSocket.emit("login", object);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Listener for login button
+     */
+    private View.OnClickListener loginListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String username = usernameInput.getText().toString();
+            String password = passwordInput.getText().toString();
+
+            if(validateInput(username, password)){
+                mUsername = username;
+                login(username, password);
             }
-        });
+        }
+    };
 
-        switchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    /**
+     * Listener for login response event
+     */
+    private Emitter.Listener onLogin = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            try {
+
+                JSONObject data = (JSONObject) args[0];
+                String response = data.getString("response");
+                Debug.print("Login response: "+response);
+
                 switchActivity();
+
+            } catch (JSONException e){
+                e.printStackTrace();
             }
-        });
+        }
+    };
 
-    }
-
-    private void login(){
-        //TODO:
-        // mSocket.emit("login", "IWANTTOLOGIN");
-    }
-
+    /**
+     * Close login activity
+     */
     private void switchActivity(){
-        Intent intent = new Intent(this, ChatTestActivity.class);
-        startActivity(intent);
+        Intent intent = new Intent();
+        intent.putExtra("username", mUsername);
+        setResult(RESULT_OK, intent);
         finish();
     }
 }
