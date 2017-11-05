a5f507ee44c242ca9eb2d37201a4fb7b47be76ca
Samuel Klein <samuel.klein@online.de>
2017-11-05 19:25:26 +0100

    added constants class to handle constants

---
+++ app/src/main/java/com/samuel/klein/randomchat/account/Constants.java
@@ -0,0 +1,11 @@
+package com.samuel.klein.randomchat.account;
+
+/**
+ * Created by sam on 05.11.17.
+ */
+
+public class Constants {
+
+    public static final String CHAT_SERVER_URL = "http://192.168.178.29:9999";
+
+}
