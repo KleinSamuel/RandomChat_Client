97532cc5fcba3b2e110b478f7316ebf7a3aca369
Samuel Klein <samuel.klein@online.de>
2017-11-07 16:21:01 +0100

    added menu fragment to be main menu and entry point of app

--- app/src/main/java/com/samuel/klein/randomchat/main/MenuFragment.java
+++ app/src/main/java/com/samuel/klein/randomchat/main/MenuFragment.java
@@ -1,80 +1,93 @@
 package com.samuel.klein.randomchat.main;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.samuel.klein.randomchat.R;
+import com.samuel.klein.randomchat.account.ChatApplication;
 import com.samuel.klein.randomchat.account.LoginActivity;
-import com.samuel.klein.randomchat.debug.Debug;
+import com.samuel.klein.randomchat.chat.MainActivity;
 
-/**
- * Created by sam on 05.11.17.
- */
+public class MenuFragment extends Fragment {
 
-public class MainFragment extends Fragment {
-
-    private static final String TAG = "MainFragment";
+    private static final String TAG = "MenuFragment";
     private static final int REQUEST_LOGIN = 0;
 
-    private TextView textview;
-
+    private MainActivity mainActivity;
+    private ChatApplication app;
     private String mUsername;
 
-    public MainFragment(){
+    private TextView usernameView;
+
+    public MenuFragment(){
         super();
-        Debug.print("Fragment constructor called!");
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Debug.print("On create called");
+        mainActivity = (MainActivity) getActivity();
+        app = mainActivity.getApp();
 
-        if(mUsername == null) {
+        /* get username from main activity */
+        mUsername = mainActivity.getUsername();
+
+        /* get to login if no username was found */
+        if(mUsername == null){
             signIn();
         }
+
+        app.setUser(mUsername);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Debug.print("On create view called!");
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_menu, container, false);
+        return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Debug.print("On view created called");
-        textview = (TextView) view.findViewById(R.id.textview);
+
+        usernameView = (TextView) view.findViewById(R.id.usernameTextView);
+
+        updateContent();
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Debug.print("On activity result called");
 
         if (Activity.RESULT_OK != resultCode) {
             getActivity().finish();
             return;
         }
         mUsername = data.getStringExtra("username");
-        textview.setText("Welcome "+mUsername);
+        mainActivity.storeUsername(mUsername);
+
+        updateContent();
+    }
+
+    private void updateContent(){
+        usernameView.setText(mUsername);
     }
 
     /**
-     * Open Login activity
+     * Open Login activity to get username
      */
     public void signIn() {
         mUsername = null;
         Intent intent = new Intent(getActivity(), LoginActivity.class);
         startActivityForResult(intent, REQUEST_LOGIN);
     }
+
 }
