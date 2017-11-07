48a7e04ad32d02229d768ef5215e710e8ed778e5
Samuel Klein <samuel.klein@online.de>
2017-11-07 23:40:18 +0100

    added blank activity for account settings

---
+++ app/src/main/java/com/samuel/klein/randomchat/settings/AccountSettingsActivity.java
@@ -0,0 +1,15 @@
+package com.samuel.klein.randomchat.settings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.samuel.klein.randomchat.R;
+
+public class AccountSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_settings);
+    }
+}
