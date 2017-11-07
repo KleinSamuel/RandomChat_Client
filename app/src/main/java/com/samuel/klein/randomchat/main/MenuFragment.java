a832fb968fa0917e2bac9ac89441381635d2cab4
Samuel Klein <samuel.klein@online.de>
2017-11-07 23:43:35 +0100

    added functionality for public chatroom list and cleaned up code

--- app/src/main/java/com/samuel/klein/randomchat/main/MenuFragment.java
+++ app/src/main/java/com/samuel/klein/randomchat/main/MenuFragment.java
@@ -5,16 +5,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.Socket;
 import com.samuel.klein.randomchat.R;
 import com.samuel.klein.randomchat.account.ChatApplication;
 import com.samuel.klein.randomchat.account.LoginActivity;
 import com.samuel.klein.randomchat.debug.Debug;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MenuFragment extends Fragment {
 
@@ -23,11 +32,14 @@ public class MenuFragment extends Fragment {
 
     private MainActivity mainActivity;
     private ChatApplication app;
+    private Socket mSocket;
     private String mUsername;
 
     /* ui elements */
     private TextView usernameView;
     private Button randomChatButton;
+    private Button roomListButton;
+    private Button privateChatButton;
 
     public MenuFragment(){
         super();
@@ -39,6 +51,9 @@ public class MenuFragment extends Fragment {
 
         mainActivity = (MainActivity) getActivity();
         app = mainActivity.getApp();
+        mSocket = app.getSocket();
+
+        mSocket.on("receiveRoomList", roomListListener);
 
         /* get username from main activity */
         mUsername = mainActivity.getUsername();
@@ -63,8 +78,12 @@ public class MenuFragment extends Fragment {
 
         usernameView = (TextView) view.findViewById(R.id.username_menu);
         randomChatButton = (Button) view.findViewById(R.id.enter_random_chat_button_menu);
+        privateChatButton = (Button) view.findViewById(R.id.enter_private_chat_button_menu);
+        roomListButton = (Button) view.findViewById(R.id.public_chatrooms_button_menu);
 
         randomChatButton.setOnClickListener(randomChatButtonListener);
+        privateChatButton.setOnClickListener(privateChatButtonListener);
+        roomListButton.setOnClickListener(roomListButtonListener);
 
         updateContent();
     }
@@ -94,11 +113,52 @@ public class MenuFragment extends Fragment {
         startActivityForResult(intent, REQUEST_LOGIN);
     }
 
+    private void requestRoomList(){
+        mSocket.emit("requestRoomList");
+    }
+
+    Emitter.Listener roomListListener = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            try {
+                JSONArray array = (JSONArray) args[0];
+                ArrayList<String> roomList = new ArrayList<>();
+
+                for(int i = 0; i < array.length(); i++){
+                    JSONObject obj = array.getJSONObject(i);
+                    String roomName = obj.getString("name");
+                    String limit = obj.getString("limit");
+                    roomList.add(roomName+"_"+limit);
+                }
+
+                mainActivity.openRoomList(roomList);
+
+            } catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+    };
+
     View.OnClickListener randomChatButtonListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Debug.print("Entering random chatroom..");
-            mainActivity.switchActivity();
+            mainActivity.requestRandomChatroom();
+        }
+    };
+
+    View.OnClickListener roomListButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Debug.print("Requesting room list..");
+            requestRoomList();
+        }
+    };
+
+    View.OnClickListener privateChatButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Debug.print("Requesting connection..");
         }
     };
 
