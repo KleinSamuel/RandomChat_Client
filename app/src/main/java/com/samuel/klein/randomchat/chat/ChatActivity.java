8d817439d38746c56295b7a5bc04dffc5c02fffc
Samuel Klein <samuel.klein@online.de>
2017-11-07 16:55:50 +0100

    created new activity for a chatroom

---
+++ app/src/main/java/com/samuel/klein/randomchat/chat/ChatActivity.java
@@ -0,0 +1,17 @@
+package com.samuel.klein.randomchat.chat;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.samuel.klein.randomchat.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_chat);
+    }
+
+}
