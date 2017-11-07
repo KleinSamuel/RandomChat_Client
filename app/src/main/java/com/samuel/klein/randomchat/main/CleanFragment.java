d245f03354ec172504567d96293aed9d2dcbde29
Samuel Klein <samuel.klein@online.de>
2017-11-07 16:19:48 +0100

    added clean fragment with all important override methods as a template

---
+++ app/src/main/java/com/samuel/klein/randomchat/main/CleanFragment.java
@@ -0,0 +1,53 @@
+package com.samuel.klein.randomchat.main;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.samuel.klein.randomchat.R;
+import com.samuel.klein.randomchat.debug.Debug;
+
+public class CleanFragment extends Fragment {
+
+    public CleanFragment(){
+        super();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Debug.print("On attach called");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Debug.print("On create called");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Debug.print("On create view called!");
+
+        View view = inflater.inflate(R.layout.fragment_menu, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Debug.print("On view created called");
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Debug.print("On activity result called");
+    }
+
+}
