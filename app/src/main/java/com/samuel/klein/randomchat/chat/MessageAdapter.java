d4203ab569e78f2e3b12b9d55e055bf829a148fb
sam <samuel.klein@online.de>
2017-11-10 22:35:06 +0100

    added new message layout

--- app/src/main/java/com/samuel/klein/randomchat/chat/MessageAdapter.java
+++ app/src/main/java/com/samuel/klein/randomchat/chat/MessageAdapter.java
@@ -2,6 +2,7 @@ package com.samuel.klein.randomchat.chat;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
